048ccbbf7404a3efa2f258fe5ebf6568f5b79711
brook110413 <brook110413@gmail.com>
2020-05-31 16:46:47 +0800

    finish pc top choices

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,24 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
+
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 4,
+//   spaceBetween: 30,
+//   freeMode: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
