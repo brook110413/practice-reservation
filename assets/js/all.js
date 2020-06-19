0362b13c67a462f18279ec07fb085883508761d8
brook110413 <brook110413@gmail.com>
2020-06-19 23:16:01 +0800

    Update 2020-06-19T15:15:30.385Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,36 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 1.4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2.4,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    }
+  });
+  var mySwiper2 = new Swiper('.swiper-container2', {
+    // Optional parameters
+    slidesPerView: 1.4,
+    spaceBetween: 30,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
